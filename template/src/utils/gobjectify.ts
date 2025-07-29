93d150a54011b5aa4255b3bf888c373e3b713233
Heliguy <heliguy4599@hotmail.com>
2025-07-29 18:32:04 -0400

    Add GObjectify

---
+++ template/src/utils/gobjectify.ts
@@ -0,0 +1,347 @@
+import GLib from "gi://GLib?version=2.0"
+import GObject from "gi://GObject?version=2.0"
+import Gio from "gi://Gio?version=2.0"
+import Gtk from "gi://Gtk?version=4.0"
+
+import { PropertyHelpers, AllPropertyTypes, PropertyConfigFor } from "./property_helpers.js"
+
+type AtleaseOneOf<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> & {
+	[K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+}[Keys]
+
+type KeysWithType<T, V> = {
+	[K in keyof T]: T[K] extends V ? K : never
+}[keyof T]
+
+type DissAllowConstantInFlags<T> = (T extends { flags?: infer F }
+	? F extends string
+		? "CONSTANT" extends F
+			? never
+			: T
+		: T
+	: T
+)
+
+type GClass<T extends GObject.Object & { $ready?: ()=> void | Promise<void> }> = (new (
+	...args: any[]
+)=> T) & { $gtype: GObject.GType }
+
+type Spec = {
+	name: string,
+	spec: GObject.ParamSpec,
+}
+
+type Signal = {
+	flags?: GObject.SignalFlags,
+	param_types?: GObject.GType[],
+	return_type?: GObject.GType,
+	accumulator?: GObject.AccumulatorType,
+}
+
+type SimpleAction = Omit<Partial<Gio.SimpleAction.ConstructorProps>, "name">
+
+type ClassDecoratorParams = AtleaseOneOf<{
+	template?: string,
+	implements?: { $gtype: GObject.GType }[],
+	css_name?: string,
+	gtype_flags?: GObject.TypeFlags,
+	manual_gtype_name?: string,
+	manual_properties?: Record<string, GObject.ParamSpec>,
+	manual_internal_children?: string[],
+}>
+
+const signals_map = new WeakMap<Function, Record<string, Signal>>()
+const property_map = new WeakMap<GObject.Object, Record<string, any>>()
+
+const CHILD_SYMBOL = Symbol("GObjectify_Decorators_Child")
+const PROP_SYMBOL = Symbol("GObjectify_Property")
+const ACTION_GROUP_SYMBOL = Symbol("GObjectify_Action_Group_Symbol")
+
+export class GObjectify {
+	public static Child<T extends GObject.Object, U extends GObject.Object>(
+		_target: ClassAccessorDecoratorTarget<T, U>,
+		context: ClassAccessorDecoratorContext<T, U>,
+	): ClassAccessorDecoratorResult<T, U> {
+		const field_name = String(context.name)
+		const gjs_name = `_${field_name}`
+		const get = function (this: T): U {
+			return (this as any)[gjs_name]
+		}
+		const set = function (this: T, _value: U): void {
+			throw new Error("Cannot write to internal children!")
+		}
+		;(get as any)[CHILD_SYMBOL] = field_name
+		return { get, set }
+	}
+
+	public static Property<T extends GObject.Object, U extends AllPropertyTypes, V>(
+		prop_type: U,
+		config?: AtleaseOneOf<PropertyConfigFor<U>>,
+	) {
+		return (
+			_target: ClassAccessorDecoratorTarget<T, V>,
+			context: ClassAccessorDecoratorContext<T, V>,
+		): ClassAccessorDecoratorResult<T, V> => {
+			const field_name = String(context.name)
+			const spec = PropertyHelpers.resolve(field_name, prop_type, config as PropertyConfigFor<U>)
+			const set = function (this: T, value: V): void {
+				if (config?.flags === "CONSTANT") {
+					throw new Error(
+						`[GObjectify]: Cannot write to CONSTANT property: '${field_name}' | for object: '${this}'`,
+					)
+				}
+				let props = property_map.get(this)
+				if (!props) {
+					props = {}
+					property_map.set(this, props)
+				}
+				props[field_name] = value
+				this.emit(`notify::${field_name.replaceAll("_", "-")}`, spec)
+			}
+			const get = function (this: T): V {
+				const my_props = property_map.get(this)
+				if (my_props) {
+					return (my_props[field_name] ?? spec.get_default_value() ?? null) as V
+				}
+				return (spec.get_default_value() ?? null) as V
+			}
+			;(get as any)[PROP_SYMBOL] = {
+				name: field_name,
+				spec,
+			} satisfies Spec
+			return { set, get }
+		}
+	}
+
+	public static CustomProp<
+		T extends GObject.Object,
+		U extends AllPropertyTypes,
+		V extends T[W],
+		W extends KeysWithType<T, V>,
+	>(
+		prop_type: U,
+		config?: AtleaseOneOf<
+			DissAllowConstantInFlags<PropertyConfigFor<U>> & {
+				initializable_backing_field?: W | (string & {}),
+			}
+		>,
+	) {
+		const backing_field: W | string | undefined = (config && "initializable_backing_field" in config
+			? config.initializable_backing_field
+			: undefined
+		)
+		return (target: (this: T)=> V, context: ClassGetterDecoratorContext<T, V>): (()=> V) => {
+			const field_name = String(context.name)
+			const spec = PropertyHelpers.resolve(field_name, prop_type, config as PropertyConfigFor<U>)
+			const get = function (this: T): V {
+				return (target.call(this) ?? spec.get_default_value() ?? null) as V
+			}
+			;(get as any)[PROP_SYMBOL] = {
+				name: field_name,
+				spec,
+			} satisfies Spec
+			if (backing_field) {
+				context.addInitializer(function (this: T) {
+					if ((this as any)[backing_field] !== undefined) return
+					;(this as any)[backing_field] = (spec.get_default_value() ?? null) as V
+				})
+			}
+			return get
+		}
+	}
+
+	public static SetterNotify<T extends GObject.Object, U>(
+		target: (this: T, arg0: U)=> void,
+		context: ClassSetterDecoratorContext,
+	): (this: T, arg0: U)=> void {
+		const field_name = String(context.name)
+		return function (this: T, value: U) {
+			target.call(this, value)
+			this.emit(`notify::${field_name.replaceAll("_", "-")}`, null)
+		}
+	}
+
+	public static SimpleAction<
+		T extends Gtk.Widget | Gtk.ApplicationWindow | Gtk.Application,
+		U extends (
+			| ((this: T)=> any)
+			| ((this: T, action: Gio.SimpleAction)=> any)
+			| ((this: T, action: Gio.SimpleAction, value: GLib.Variant)=> any)
+		),
+		K extends KeysWithType<T, Gio.SimpleAction>,
+	>(params?: AtleaseOneOf<SimpleAction & {
+		save_to?: K,
+		accels?: T extends Gtk.Application ? string[] : never,
+	}>) {
+		const { save_to, accels, ...config } = params ?? {}
+		return (target: U, context: ClassMethodDecoratorContext<T, U>): void => {
+			const field_name = String(context.name)
+			context.addInitializer(function (this: T) {
+				const action = new Gio.SimpleAction({ name: String(context.name), ...config })
+				action.connect("activate", target.bind(this))
+
+				const is_widget = this instanceof Gtk.Widget
+				const is_window = this instanceof Gtk.ApplicationWindow
+				const is_applic = this instanceof Gtk.Application
+
+				if (is_widget) {
+					let group: Gio.SimpleActionGroup | undefined = (this as any)[ACTION_GROUP_SYMBOL]
+					if (!group) {
+						group = new Gio.SimpleActionGroup()
+						;(this as any)[ACTION_GROUP_SYMBOL] = group
+						this.insert_action_group(this.constructor.name, group)
+					}
+					group.add_action(action)
+				} else if (is_window || is_applic) {
+					this.add_action(action)
+					if (is_applic && accels) this.set_accels_for_action(`app.${field_name}`, accels)
+				}
+				if (save_to) {
+					Object.defineProperty(this, save_to, {
+						value: action,
+						writable: false,
+						enumerable: true,
+						configurable: true,
+					})
+				}
+			})
+		}
+	}
+
+	public static Debounce<T extends GObject.Object, U extends (this: T, ...args: any[])=> void>(
+		milliseconds: number,
+		params: { trigger: "leading" | "trailing" | "leading+trailing" } = { trigger: "trailing" },
+	) {
+		const leading = params.trigger.includes("leading")
+		const trailing = params.trigger.includes("trailing")
+		return (original_method: U, context: ClassMethodDecoratorContext): U => {
+			const timeout_symbol = Symbol(`DebounceDebouncerFor${context.name.toString()}`)
+			const last_args_symbol = Symbol(`DebounceDebounceArgsFor${context.name.toString()}`)
+			const should_call_trailing_symbol = Symbol(
+				`DebounceShouldCallTrailingFor${context.name.toString()}`,
+			)
+			const debounced = function (this: T, ...args: any[]): void {
+				const has_scheduled = (this as any)[timeout_symbol] != null
+				if (leading && !has_scheduled) {
+					original_method.apply(this, args)
+				} else {
+					(this as any)[last_args_symbol] = args
+					;(this as any)[should_call_trailing_symbol] = true
+				}
+				if ((this as any)[timeout_symbol]) {
+					GLib.source_remove((this as any)[timeout_symbol])
+				}
+				(this as any)[timeout_symbol] = GLib.timeout_add(
+					GLib.PRIORITY_DEFAULT,
+					milliseconds,
+					() => {
+						(this as any)[timeout_symbol] = null
+						if (trailing && (this as any)[should_call_trailing_symbol]) {
+							original_method.apply(this, (this as any)[last_args_symbol] ?? [])
+							;(this as any)[should_call_trailing_symbol] = false
+						}
+						return GLib.SOURCE_REMOVE
+					},
+				)
+			}
+			return debounced as U
+		}
+	}
+
+	public static Signal<T extends GObject.Object>(name: string, props?: AtleaseOneOf<Signal>) {
+		return (target: GClass<T>, _context: ClassDecoratorContext): void => {
+			let signals = signals_map.get(target)
+			if (!signals) {
+				signals = {}
+				signals_map.set(target, signals)
+			}
+			signals[name] = props ?? {}
+		}
+	}
+
+	public static Class<T extends GObject.Object & { $ready?: ()=> void | Promise<void> }>(
+		params?: ClassDecoratorParams,
+	) {
+		return (target: GClass<T>, _context: ClassDecoratorContext): void => {
+			const prototype = target.prototype
+			const children: string[] = []
+			const specs: Record<string, GObject.ParamSpec> = {}
+			for (const key of Object.getOwnPropertyNames(prototype)) {
+				const descriptor = Object.getOwnPropertyDescriptor(prototype, key)
+				const getter = descriptor?.get ?? {}
+
+				const child_ui_name = (getter as any)[CHILD_SYMBOL]
+				if (child_ui_name && typeof child_ui_name === "string") children.push(child_ui_name)
+
+				const prop_spec: Spec | undefined = (getter as any)[PROP_SYMBOL]
+				if (prop_spec && prop_spec.spec instanceof GObject.ParamSpec) {
+					specs[prop_spec.name] = prop_spec.spec
+				}
+			}
+			if (params?.manual_internal_children) {
+				params.manual_internal_children.forEach((item) => children.push(item))
+			}
+			GObject.registerClass({
+				...(params?.template && { Template: `resource://${params.template}.ui` }),
+				GTypeName: params?.manual_gtype_name || target.name,
+				InternalChildren: children,
+				Signals: signals_map.get(target) ?? {},
+				...(params?.css_name && { CssName: params.css_name }),
+				Properties: {
+					...specs,
+					...(params?.manual_properties && params.manual_properties),
+				},
+			}, target)
+			signals_map.delete(target)
+
+			const ready = target.prototype.$ready
+			if (typeof ready !== "function") return
+			const original_init = target.prototype._init
+			target.prototype._init = function (...args: any): any {
+				const original_return_val = original_init?.apply(this, args)
+				GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+					const on_error = (e: any): void => {
+						print(`Error in $ready function for ${target.name}`)
+						print(e)
+					}
+					try {
+						const return_val = ready.call(this)
+						if (return_val instanceof Promise) return_val.catch(on_error)
+					} catch (e) {
+						on_error(e)
+					}
+					return GLib.SOURCE_REMOVE
+				})
+				return original_return_val
+			}
+		}
+	}
+
+	public static async_connect<Args extends unknown[] = []>(
+		obj: GObject.Object,
+		resolve_signal: string,
+		reject_signal?: string,
+	): Promise<Args> {
+		return new Promise((resolve, reject) => {
+			let resolve_id: number | null = null
+			let reject_id: number | null = null
+			const cleanup = (): void => {
+				if (resolve_id !== null) obj.disconnect(resolve_id)
+				if (reject_id !== null) obj.disconnect(reject_id)
+			}
+
+			resolve_id = obj.connect(resolve_signal, (_obj, ...args: Args) => {
+				cleanup()
+				resolve(args)
+			})
+
+			if (!reject_signal) return
+			reject_id = obj.connect(reject_signal, (_obj, ...args: any) => {
+				cleanup()
+				reject(new Error(`Rejection signal: '${reject_signal}' triggered with args: ${args}`))
+			})
+		})
+	}
+
+	private constructor() {}
+}
---
+++ template/src/utils/property_helpers.ts
@@ -0,0 +1,207 @@
+import GObject from "gi://GObject?version=2.0"
+import GLib from "gi://GLib?version=2.0"
+
+type GClass = (new (...args: any[])=> GObject.Object) & { $gtype: GObject.GType }
+type GEnum<T = number> = { $gtype: GObject.GType<T> }
+
+const FLAG_PRESETS = {
+	CONSTANT: GObject.ParamFlags.READABLE,
+	READWRITE: GObject.ParamFlags.READWRITE,
+	CONSTRUCT: GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT,
+	CONSTRUCT_ONLY: GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+} as const
+
+type ParamFlagStrings = keyof typeof FLAG_PRESETS
+
+type BaseSpecParams = {
+	nick?: string,
+	blurb?: string,
+	flags?: ParamFlagStrings,
+}
+
+type DefaultableBaseSpecParams<T> = BaseSpecParams & {
+	default?: T,
+}
+
+type NumberSpecParams = DefaultableBaseSpecParams<number> & {
+	min?: number,
+	max?: number,
+}
+
+type StringSpecParams = DefaultableBaseSpecParams<string>
+
+type BoolSpecParams = DefaultableBaseSpecParams<boolean>
+
+type PropertyTypeMap = {
+	string: StringSpecParams,
+	bool: BoolSpecParams,
+	uint32: NumberSpecParams,
+	int32: NumberSpecParams,
+	double: NumberSpecParams,
+}
+
+export type AllPropertyTypes = GClass | GEnum | keyof PropertyTypeMap
+
+export type PropertyConfigFor<T extends AllPropertyTypes> = (T extends GClass
+	? BaseSpecParams
+	: (T extends GEnum<infer U>
+		? DefaultableBaseSpecParams<U>
+		: (T extends keyof PropertyTypeMap
+			? PropertyTypeMap[T]
+			: never
+		)
+	)
+)
+
+function title_case(str: string): string {
+	str = str.replace(/[_-]/g, " ")
+	return str.replace(
+		/\w\S*/g,
+		(word) => word.charAt(0).toUpperCase() + word.substr(1).toLowerCase(),
+	)
+}
+
+function get_defaults(name: string, params?: BaseSpecParams): Required<BaseSpecParams> {
+	const nick = title_case(params?.nick ?? name)
+	return {
+		nick,
+		blurb: params?.blurb ?? `${nick} property`,
+		flags: params?.flags ?? "READWRITE",
+	}
+}
+
+function string_to_flag(flag_string: ParamFlagStrings): GObject.ParamFlags {
+	return FLAG_PRESETS[flag_string]
+}
+
+function is_GObject(value: any): value is GClass {
+	return (
+		typeof value === "function"
+		&& typeof value.$gtype?.name === "string"
+		&& typeof value.prototype === "object"
+	)
+}
+
+function is_GEnum(value: any): value is GEnum {
+	return (
+		typeof value === "object" && typeof value.$gtype?.name === "string" && !("prototype" in value)
+	)
+}
+
+export class PropertyHelpers {
+	public static string(name: string, options?: StringSpecParams): GObject.ParamSpec<string> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		return GObject.ParamSpec.string(
+			name,
+			nick,
+			blurb,
+			string_to_flag(flags),
+			options?.default ?? "",
+		)
+	}
+
+	public static bool(name: string, options?: BoolSpecParams): GObject.ParamSpec<boolean> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		return GObject.ParamSpec.boolean(
+			name,
+			nick,
+			blurb,
+			string_to_flag(flags),
+			options?.default ?? false,
+		)
+	}
+
+	public static uint32(name: string, options?: NumberSpecParams): GObject.ParamSpec<number> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		const min = options?.min ?? 0
+		return GObject.ParamSpec.uint(
+			name,
+			nick,
+			blurb,
+			string_to_flag(flags),
+			min,
+			options?.max ?? GLib.MAXUINT32,
+			options?.default ?? min,
+		)
+	}
+
+	public static int32(name: string, options?: NumberSpecParams): GObject.ParamSpec<number> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		const min = options?.min ?? 0
+		return GObject.ParamSpec.int(
+			name,
+			nick,
+			blurb,
+			string_to_flag(flags),
+			min,
+			options?.max ?? GLib.MAXINT32,
+			options?.default ?? min,
+		)
+	}
+
+	public static double(name: string, options?: NumberSpecParams): GObject.ParamSpec<number> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		const min = options?.min ?? 0
+		return GObject.ParamSpec.double(
+			name,
+			nick,
+			blurb,
+			string_to_flag(flags),
+			min,
+			options?.max ?? Number.MAX_VALUE,
+			options?.default ?? min,
+		)
+	}
+
+	public static g_object<T extends GClass>(
+		name: string,
+		type: T,
+		options?: BaseSpecParams,
+	): GObject.ParamSpec<T> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		return GObject.ParamSpec.object(
+			name,
+			nick,
+			blurb,
+			string_to_flag(flags) as unknown as string,
+			type.$gtype,
+		)
+	}
+
+	public static g_enum<T extends GEnum<any>>(
+		name: string,
+		type: T,
+		options?: DefaultableBaseSpecParams<T>,
+	): GObject.ParamSpec<T> {
+		const { nick, blurb, flags } = get_defaults(name, options)
+		return GObject.ParamSpec.enum(name, nick, blurb, string_to_flag(flags), type, options?.default)
+	}
+
+	public static make_props_object(props: GObject.ParamSpec[]): Record<string, GObject.ParamSpec> {
+		const to_return: Record<string, GObject.ParamSpec> = {}
+		props.forEach((prop) => (to_return[prop.name] = prop))
+		return to_return
+	}
+
+	public static resolve<T extends AllPropertyTypes>(
+		name: string,
+		type: T,
+		config?: PropertyConfigFor<T>,
+	): GObject.ParamSpec<any> {
+		if (is_GObject(type)) {
+			return this.g_object(name, type, config)
+		} else if (is_GEnum(type)) {
+			return this.g_enum(name, type, config as DefaultableBaseSpecParams<GEnum<any>>)
+		}
+		switch (type) {
+			case "string": return this.string(name, config as StringSpecParams)
+			case "bool": return this.bool(name, config as BoolSpecParams)
+			case "uint32": return this.uint32(name, config as NumberSpecParams)
+			case "int32": return this.int32(name, config as NumberSpecParams)
+			case "double": return this.double(name, config as NumberSpecParams)
+		}
+		throw new Error("PropertyHelpers.resolve: Unsupported property type. Called with: " + type)
+	}
+
+	private constructor() {}
+}
