54ab65589b7f21bb002a86f93cb57a54924369d2
Heliguy <heliguy4599@hotmail.com>
2025-07-28 22:53:43 -0400

    Expose PROFILE and APP_ID Meson variables to the TS code through the global pkg object, declaring its typing to match

--- template/types/ambient.d.ts
+++ template/types/ambient.d.ts
@@ -4,8 +4,16 @@ declare function log(obj: object, others?: object[]): void
 declare function log(msg: string, substitutions?: any[]): void
 
 declare const pkg: {
-	version: string
+	app_id: string
+	datadir: string
+	libdir: string
+	localedir: string
+	moduledir: string
 	name: string
+	pkgdatadir: string
+	prefix: string
+	profile: "default" | "development"
+	version: string
 }
 
 declare namespace console {
