121c00a42f4bd05a695ec53a9052c10bcd4a4ddf
Heliguy <heliguy4599@hotmail.com>
2025-07-29 01:43:08 -0400

    Remove config JSON and TS files, opting instead for defining needed fields on the global pkg object

--- template/src/main.ts
+++ template/src/main.ts
@@ -3,18 +3,17 @@ import Gio from "gi://Gio?version=2.0"
 import Adw from "gi://Adw?version=1"
 
 import { MainWindow } from "./window/main_window.js"
-import { Config } from "./config.js"
 
 export class Application extends Adw.Application {
 	static {
 		GObject.registerClass(this)
 	}
 
-	main_window?: MainWindow
+	private main_window?: MainWindow
 
-	constructor() {
+	public constructor() {
 		super({
-			application_id: Config.APP_ID,
+			application_id: pkg.app_id,
 			flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
 		})
 
@@ -25,20 +24,18 @@ export class Application extends Adw.Application {
 		this.set_accels_for_action("app.quit", ["<primary>q"])
 
 		const show_about_action = new Gio.SimpleAction({ name: "about" })
-		show_about_action.connect("activate", () =>
-			new Adw.AboutDialog({
-				application_name: "<TEMPLATE:APP_TITLE>",
-				application_icon: Config.APP_ID,
-				developer_name: "<TEMPLATE:DEVELOPER_NAME>",
-				version: Config.VERSION,
-				developers: ["<TEMPLATE:DEVELOPER_NAME> <<TEMPLATE:DEVELOPER_EMAIL>>"],
-				copyright: "© 2025 <TEMPLATE:DEVELOPER_NAME>",
-			}).present(this.active_window),
-		)
+		show_about_action.connect("activate", () => new Adw.AboutDialog({
+			application_name: "<TEMPLATE:APP_TITLE>",
+			application_icon: pkg.app_id,
+			developer_name: "<TEMPLATE:DEVELOPER_NAME>",
+			version: pkg.version,
+			developers: ["<TEMPLATE:DEVELOPER_NAME> <<TEMPLATE:DEVELOPER_EMAIL>>"],
+			copyright: "© 2025 <TEMPLATE:DEVELOPER_NAME>",
+		}).present(this.active_window))
 		this.add_action(show_about_action)
 	}
 
-	override vfunc_activate(): void {
+	public override vfunc_activate(): void {
 		if (!this.main_window) {
 			this.main_window = new MainWindow({ application: this })
 		}
--- template/src/window/main_window.ts
+++ template/src/window/main_window.ts
@@ -2,8 +2,6 @@ import GObject from "gi://GObject?version=2.0"
 import Gio from "gi://Gio?version=2.0"
 import Adw from "gi://Adw?version=1"
 
-import { Config } from "../config.js"
-
 export class MainWindow extends Adw.ApplicationWindow {
 	static {
 		GObject.registerClass(
@@ -16,21 +14,16 @@ export class MainWindow extends Adw.ApplicationWindow {
 		)
 	}
 
-	readonly toolbar_view!: Adw.ToolbarView
-	readonly header_bar!: Adw.HeaderBar
-	readonly status_page!: Adw.StatusPage
+	protected readonly toolbar_view!: Adw.ToolbarView
+	protected readonly header_bar!: Adw.HeaderBar
+	protected readonly status_page!: Adw.StatusPage
 
-	readonly settings: Gio.Settings
+	protected readonly settings = new Gio.Settings({ schema_id: pkg.app_id })
 
-	constructor(params?: Partial<Adw.ApplicationWindow.ConstructorProps>) {
+	public constructor(params?: Partial<Adw.ApplicationWindow.ConstructorProps>) {
 		super(params)
 
-		if (Config.PROFILE === "development") {
-			this.add_css_class("devel")
-		}
-
-		this.settings = new Gio.Settings({ schema_id: Config.APP_ID })
-
-		print(`Welcome to ${Config.APP_ID}!`)
+		if (pkg.profile === "development") this.add_css_class("devel")
+		print(`Welcome to ${pkg.app_id}!`)
 	}
 }
