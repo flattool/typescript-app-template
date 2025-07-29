39fe553888b89472885fb3a3cdc929352689bc38
Heliguy <heliguy4599@hotmail.com>
2025-07-29 18:32:11 -0400

    Use GObjectify

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,46 +1,37 @@
-import GObject from "gi://GObject?version=2.0"
 import Gio from "gi://Gio?version=2.0"
 import Adw from "gi://Adw?version=1"
 
+import { GObjectify } from "./utils/gobjectify.js"
 import { MainWindow } from "./window/main_window.js"
 
+@GObjectify.Class({ manual_gtype_name: "Gjs_Application" })
 export class Application extends Adw.Application {
-	static {
-		GObject.registerClass(this)
-	}
-
-	private main_window?: MainWindow
+	private _main_window?: MainWindow
 
 	public constructor() {
 		super({
 			application_id: pkg.app_id,
 			flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
 		})
+	}
 
-		// Actions
-		const quit_action = new Gio.SimpleAction({ name: "quit" })
-		quit_action.connect("activate", () => this.quit())
-		this.add_action(quit_action)
-		this.set_accels_for_action("app.quit", ["<primary>q"])
+	@GObjectify.SimpleAction({ accels: ["<primary>q"] })
+	public override quit(): void { super.quit() }
 
-		const show_about_action = new Gio.SimpleAction({ name: "about" })
-		show_about_action.connect("activate", () => new Adw.AboutDialog({
+	@GObjectify.SimpleAction()
+	protected about(): void {
+		new Adw.AboutDialog({
 			application_name: "<TEMPLATE:APP_TITLE>",
 			application_icon: pkg.app_id,
 			developer_name: "<TEMPLATE:DEVELOPER_NAME>",
 			version: pkg.version,
 			developers: ["<TEMPLATE:DEVELOPER_NAME> <<TEMPLATE:DEVELOPER_EMAIL>>"],
 			copyright: "© 2025 <TEMPLATE:DEVELOPER_NAME>",
-		}).present(this.active_window))
-		this.add_action(show_about_action)
+		}).present(this.active_window)
 	}
 
 	public override vfunc_activate(): void {
-		if (!this.main_window) {
-			this.main_window = new MainWindow({ application: this })
-		}
-
-		this.main_window.present()
+		(this._main_window ??= new MainWindow({ application: this })).present()
 	}
 }
 
--- template/src/window/main_window.ts
+++ template/src/window/main_window.ts
@@ -1,23 +1,10 @@
-import GObject from "gi://GObject?version=2.0"
 import Gio from "gi://Gio?version=2.0"
 import Adw from "gi://Adw?version=1"
 
+import { GObjectify } from "../utils/gobjectify.js"
+
+@GObjectify.Class({ template: "/<TEMPLATE:APPID.as_path>/window/main_window" })
 export class MainWindow extends Adw.ApplicationWindow {
-	static {
-		GObject.registerClass(
-			{
-				GTypeName: "MainWindow",
-				Template: "resource:///<TEMPLATE:APPID.as_path>/window/main_window.ui",
-				InternalChildren: ["toolbar_view", "header_bar", "status_page"],
-			},
-			this,
-		)
-	}
-
-	protected readonly toolbar_view!: Adw.ToolbarView
-	protected readonly header_bar!: Adw.HeaderBar
-	protected readonly status_page!: Adw.StatusPage
-
 	protected readonly settings = new Gio.Settings({ schema_id: pkg.app_id })
 
 	public constructor(params?: Partial<Adw.ApplicationWindow.ConstructorProps>) {
