3727c7d70d448b3c24f91c9234ea80658159e76f
heliguy4599 <heliguy4599@hotmail.com>
2025-11-27 00:00:19 -0500

    Update to use the new GObjectify

--- template/src/main.ts
+++ template/src/main.ts
@@ -3,38 +3,39 @@ import Gio from "gi://Gio?version=2.0"
 import Gtk from "gi://Gtk?version=4.0"
 import Adw from "gi://Adw?version=1"
 
-import { GObjectify } from "./utils/gobjectify.js"
+import { GClass, SimpleAction, OnSimpleAction, from, dedent } from "./gobjectify/gobjectify.js"
 import { MainWindow } from "./window/main_window.js"
 
-@GObjectify.Class({ manual_gtype_name: "Gjs_Application" })
-export class Application extends Adw.Application {
-	private _main_window?: MainWindow
+@GClass({ manual_gtype_name: "Gjs_Application" })
+export class Application extends from(Adw.Application, {
+	_quit: SimpleAction({ accels: ["<primary>q"] }),
+	about: SimpleAction(),
+}) {
+	#main_window?: MainWindow
 
-	public constructor() {
-		super({
-			application_id: pkg.app_id,
-			flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
-		})
+	override vfunc_activate(): void {
+		(this.#main_window ??= new MainWindow({ application: this })).present()
 	}
 
-	@GObjectify.SimpleAction({ accels: ["<primary>q"] })
-	public override quit(): void { super.quit() }
+	_ready(): void {
+		this._quit.connect("activate", () => this.quit())
+	}
 
-	@GObjectify.SimpleAction()
+	@OnSimpleAction("about")
 	protected about(): void {
 		const gtk_version = `${Gtk.MAJOR_VERSION}.${Gtk.MINOR_VERSION}.${Gtk.MICRO_VERSION}`
 		const adw_version = `${Adw.MAJOR_VERSION}.${Adw.MINOR_VERSION}.${Adw.MICRO_VERSION}`
 		const os_string = `${GLib.get_os_info("NAME")} ${GLib.get_os_info("VERSION")}`
 		const lang = GLib.environ_getenv(GLib.get_environ(), "LANG")
-		const troubleshooting = (
-			`OS: ${os_string}\n`
-			+ `{{APP_NAME}} version: ${pkg.version}\n`
-			+ `GTK: ${gtk_version}\n`
-			+ `libadwaita: ${adw_version}\n`
-			+ `App ID: ${pkg.app_id}\n`
-			+ `Profile: ${pkg.profile}\n`
-			+ `Language: ${lang}`
-		)
+		const troubleshooting = dedent`
+			OS: ${os_string}
+			an-app version: ${pkg.version}
+			GTK: ${gtk_version}
+			libadwaita: ${adw_version}
+			App ID: ${pkg.app_id}
+			Profile: ${pkg.profile}
+			Language: ${lang}
+		`
 
 		const dialog = Adw.AboutDialog.new_from_appdata("/{{APP_ID_AS_PATH}}/appdata", null)
 		dialog.version = pkg.version
@@ -42,13 +43,12 @@ export class Application extends Adw.Application {
 		{{#ifset DONATION_LINK}}dialog.add_link(_("Donate"), "{{DONATION_LINK}}")
 		{{/}}dialog.present(this.active_window)
 	}
-
-	public override vfunc_activate(): void {
-		(this._main_window ??= new MainWindow({ application: this })).present()
-	}
 }
 
 export function main(argv: string[]): Promise<number> {
-	const app = new Application()
+	const app = new Application({
+		application_id: pkg.app_id,
+		flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
+	})
 	return app.runAsync(argv)
 }
--- template/src/window/main_window.ts
+++ template/src/window/main_window.ts
@@ -1,15 +1,13 @@
 import Gio from "gi://Gio?version=2.0"
 import Adw from "gi://Adw?version=1"
 
-import { GObjectify } from "../utils/gobjectify.js"
+import { GClass } from "../gobjectify/gobjectify.js"
 
-@GObjectify.Class({ template: "/{{APP_ID_AS_PATH}}/window/main_window" })
+@GClass({ template: "resource:///{{APP_ID_AS_PATH}}/window/main_window.ui" })
 export class MainWindow extends Adw.ApplicationWindow {
-	protected readonly settings = new Gio.Settings({ schema_id: pkg.app_id })
+	readonly #settings = new Gio.Settings({ schema_id: pkg.app_id })
 
-	public constructor(params?: Partial<Adw.ApplicationWindow.ConstructorProps>) {
-		super(params)
-
+	_ready(): void {
 		if (pkg.profile === "development") this.add_css_class("devel")
 		print(`Welcome to ${pkg.app_id}!`)
 	}
